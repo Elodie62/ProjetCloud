cca99709190d24c04ff2ab905369bbd8ea55627d
Elodie62 <elodie602@free.fr>
2022-03-07 00:58:38 +0100

    add photo drag and style

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ let dragfile = document.getElementById("addMedia");
 let inputName = document.getElementById("photoName");
 let inputDate = document.getElementById("photoDate");
 let div = document.querySelector(".formPart2");
+const imgPreview = document.getElementById("img-preview");
 
 dragfile.addEventListener("change", function () {
-  // console.log(dragfile.files[0]);
   inputName.value = dragfile.files[0].name;
   inputDate.valueAsDate = new Date();
   div.classList.remove("hidden");
+  getImgData();
 });
+
+function getImgData() {
+  const files = addMedia.files[0];
+  if (files) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(files);
+    fileReader.addEventListener("load", function () {
+      imgPreview.style.display = "block";
+      imgPreview.innerHTML = '<img src="' + this.result + '" />';
+    });
+  }
+}
